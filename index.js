f3d0cad1ef8d04265b765fbfc79c93c80f2c8e76
satiseven7 <76160836+satiseven7@users.noreply.github.com>
2021-07-07 11:42:51 +0300

    Update index.js
    change length to 8

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const createPassword = require("./utils/createPassword");
 program.program
   .version("1.0.0")
   .description("Simple Password Generator written in Nodejs For terminal usage")
-  .option("-l,--length <number>", "Length of Password", 10)
+  .option("-l,--length <number>", "Length of Password", 8)
   .option("-s,--save", "save password to text file")
   .option("-nn,--no-numbers", "remove numbers")
   .option("-ns,--no-symbols", "remove symbols")
